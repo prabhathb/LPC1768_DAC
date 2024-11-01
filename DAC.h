2256b126b0820f2432a94c499761de401f6bbeee
Prabhath Buddhika <buddhikarap@gmail.com>
2024-11-01 14:50:40 +0530

    Add files via upload
    This file is the C header file to be used for the DAC library on LPC1768 by NXP.

---
+++ DAC.h
@@ -0,0 +1,14 @@
+/*
+Prabhath Budhika
+26/10/2016
+*/
+
+#include <LPC17xx.h>
+
+extern void DAC_Init( void );
+extern void DAC_turn_ON( void );
+extern void DAC_turn_OFF( void );
+extern void DAC_high_speed ( void );
+extern void DAC_low_speed ( void );
+extern void DAC_update ( uint16_t value );
+
